9351e0d4069dbfe8eec282cf209cc0bbdabc6115
xtottel <xtottel@gmail.com>
2025-07-27 10:54:25 +0000

    New Dashboard

---
+++ src/app/dashboard/sms/templates/page.tsx
@@ -0,0 +1,10 @@
+//reponsive design testing
+
+export default function DocsHome() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-white">
+      <h1 className="text-4xl font-bold text-center mb-4">Welcome to Xtopay Docs</h1>
+      <p className="text-lg text-gray-700">Explore our documentation to get started.</p>
+    </div>
+  );
+}
